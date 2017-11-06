70607866aeb84ffb7336353e94cb541db8c501a3
nqx2007 <nqx2007@users.noreply.github.com>
2017-11-06 18:36:27 +0800

    Create stockRightPro.js

---
+++ public/scripts/invertorService/stockRightPro.js
@@ -0,0 +1,22 @@
+/**
+ * Created by niuqingxia on 2017/11/6.
+ */
+var stockRightPro=stockRightPro ||{};
+stockRightPro.renderProList=function(data){
+    var proList=document.getElementById("stockRightPro-area").innerHTML;
+    var content=ejs.render(proList,{data:data});
+    $(".proList-area").html(content);
+}
+$(function(){
+    var itemList=[
+        {url:'../../public/images/index/project.jpg',name:'项目一',time:'2001-10-12',content:'hahahahahahhh'},
+        {url:'../../public/images/index/project.jpg',name:'项目二',time:'2001-10-12',content:'hahahahahahhh'},
+        {url:'../../public/images/index/project.jpg',name:'项目三',time:'2001-10-12',content:'hahahahahahhh'},
+        {url:'../../public/images/index/project.jpg',name:'项目一',time:'2001-10-12',content:'hahahahahahhh'},
+        {url:'../../public/images/index/project.jpg',name:'项目四',time:'2001-10-12',content:'hahahahahahhh'},
+        {url:'../../public/images/index/project.jpg',name:'项目五',time:'2001-10-12',content:'hahahahahahhh'},
+        {url:'../../public/images/index/project.jpg',name:'项目六',time:'2001-10-12',content:'hahahahahahhh'},
+        {url:'../../public/images/index/project.jpg',name:'项目七',time:'2001-10-12',content:'hahahahahahhh'},
+    ];//假数据，需要使用ajax从后台获取
+    stockRightPro.renderProList(itemList);
+})
